b6e110e2cee371c21ca3e2ad0cbcc83de8d0cda7
m_najafi <m_najafi>
2017-05-02 14:07:23 +0430

    edit

--- source/dashboard/pages/admin/editcontent/editcontent.controller.js
+++ source/dashboard/pages/admin/editcontent/editcontent.controller.js
@@ -9,17 +9,30 @@
   function EditContentController() {
 
     var vm = this;
+    vm.edit = edit;
+    vm.remove = remove;
 
     vm.news = [{
-      'id': 1,
-      'title': 'news1',
-      'datetime': '1396/06/03'
-    },
-    {
-      'id': 2,
-      'title': 'news2',
-      'datetime': '1396/08/08'
-    }];
+        'id': 1,
+        'title': 'news1',
+        'datetime': '1396/06/03'
+      },
+      {
+        'id': 2,
+        'title': 'news2',
+        'datetime': '1396/08/08'
+      }
+    ];
+
+    function edit(id) {
+
+      console.log(id);
+    }
+
+    function remove() {
+
+
+    }
 
 
 
