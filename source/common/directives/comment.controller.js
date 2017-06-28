5b3cb0f41234c184640d2300518365f0ab9407c5
m_najafi <m_najafi>
2017-06-28 15:14:26 +0430

    comment

--- source/common/directives/comment.controller.js
+++ source/common/directives/comment.controller.js
@@ -2,11 +2,13 @@
 
 
   angular
-        .module('CommonApplication')
-        .controller('CommentController', [ '$scope', 'CommentRest', CommentController ]);
+    .module('CommonApplication')
+    .controller('CommentController', ['$scope', 'CommentRest', CommentController]);
 
   function CommentController($scope, CommentRest) {
     var vm = this;
+    vm.comment = {};
+    vm.comment.context = vm.comment.name = vm.comment.email = '';
     vm.add = add;
     vm.getComment = getComments;
 
@@ -19,8 +21,8 @@
         context: vm.comment.context,
         newsId: $scope.newsId
       };
-      if (vm.comment.context === '') {
-        alert('نظر خود را وارد نمایید');
+      if (vm.comment.context == '' || vm.comment.email == '' || vm.comment.name == '') {
+        alert('لطفا همه موارد را وارد نمایید');
       } else {
         CommentRest.post(newComment).then(function () {
           alert('نظر شما با موفقیت ثبت شد.!');
